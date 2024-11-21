d1cafb075fe97736b30cdc9fedd9888212ddba7e
CESAR NERI SANCHEZ <cesarsanchez678@outlook.es>
2024-11-21 15:41:40 -0600

    Cambios

--- Login/login.php
+++ Login/login.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="en">
+<html lang="es">
 <head>
   <title>Login | Family </title>
   <meta charset="utf-8">
--- Login/reset_password.php
+++ Login/reset_password.php
@@ -7,30 +7,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email'])) {
     $confirmPassword = $_POST['confirm_password'];
 
     if ($newPassword === $confirmPassword) {
-        // Encriptar la nueva contraseña
         $hashedPassword = password_hash($newPassword, PASSWORD_DEFAULT);
 
-        // Preparar dos declaraciones para actualizar la contraseña
         $stmt1 = $con->prepare("UPDATE clientes SET contrasena = ? WHERE correo_electronico = ?");
         $stmt1->bind_param("ss", $hashedPassword, $email);
         
         $stmt2 = $con->prepare("UPDATE empleados SET contrasena = ? WHERE correo_personal = ?");
         $stmt2->bind_param("ss", $hashedPassword, $email);
 
-        // Ejecutar la actualización en ambas tablas
         $updatedCliente = $stmt1->execute();
         $updatedEmpleado = $stmt2->execute();
 
-        // Verifica si al menos una actualización fue exitosa
         if ($updatedCliente || $updatedEmpleado) {
-            // Redirigir al usuario al inicio de sesión
             header("Location: login.php");
             exit();
         } else {
             echo "Hubo un problema al restablecer la contraseña.";
         }
         
-        // Cerrar las declaraciones
         $stmt1->close();
         $stmt2->close();
     } else {
